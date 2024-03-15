805310c2ad7325350d03051047242ec0695d8df8
bellusfennec <bellusfennec@gmail.com>
2024-03-15 08:42:24 +0300

    first commit

---
+++ scripts/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore, combineReducers } from "../../node_modules/redux/es/redux.mjs";
+import { productsReducer } from "./productsReducer.js";
+
+export const store = createStore(
+    combineReducers({
+        products: productsReducer
+    })
+)
